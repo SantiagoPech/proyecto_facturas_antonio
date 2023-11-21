c4e89237a3bb00d6ac7e885d2f2b32afc55bb97f
Santiago <santiago.pechalamilla@cbtis72.edu.mx>
2023-11-21 11:28:26 -0500

    Primer commit

---
+++ src/main/java/net/AntonioSantiago/com/controlDeFacturas/ControlDeFacturasApplication.java
@@ -0,0 +1,13 @@
+package net.AntonioSantiago.com.controlDeFacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/AntonioSantiago/com/controlDeFacturas/ControlDeFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package net.AntonioSantiago.com.controlDeFacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlDeFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
